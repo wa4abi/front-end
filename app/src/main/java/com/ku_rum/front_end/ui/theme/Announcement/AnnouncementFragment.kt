455127624e05887df7868176e003f68aceea81eb
wa4abi <heukimjalatte@gmail.com>
2024-12-02 00:19:20 +0900

    Fix: Announcement

--- app/src/main/java/com/ku_rum/front_end/ui/theme/Announcement/AnnouncementFragment.kt
+++ app/src/main/java/com/ku_rum/front_end/ui/theme/Announcement/AnnouncementFragment.kt
@@ -5,8 +5,8 @@ import com.kakao.sdk.common.util.Utility
 import com.ku_rum.front_end.BaseFragment
 import com.ku_rum.front_end.databinding.FragmentAnnouncementBinding
 
-class AnnouncementFragment : BaseFragment<FragmentAnnouncementBinding>(FragmentAnnouncementBinding::inflate){
 
+class AnnouncementFragment : BaseFragment<FragmentAnnouncementBinding>(FragmentAnnouncementBinding::inflate) {
     override fun initAfterBinding() {
         var keyHash = Utility.getKeyHash(requireContext())
 
