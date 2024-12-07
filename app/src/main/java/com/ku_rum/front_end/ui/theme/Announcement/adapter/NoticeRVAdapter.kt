06ec12fb9db09f1888e8872c2899e74ea778cff8
sh1220 <61071228+sh1220@users.noreply.github.com>
2024-12-07 15:07:42 +0900

    Revert "Fix: Announcement"

--- app/src/main/java/com/ku_rum/front_end/ui/theme/Announcement/adapter/NoticeRVAdapter.kt
+++ app/src/main/java/com/ku_rum/front_end/ui/theme/Announcement/adapter/NoticeRVAdapter.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.ku_rum.front_end.databinding.ItemAnnouncementImportantBinding
-import com.ku_rum.front_end.databinding.ItemAnnouncementNormalBinding
+import com.ku_rum.front_end.databinding.ItemNoticeImportantBinding
+import com.ku_rum.front_end.databinding.ItemNoticeNormalBinding
 import com.ku_rum.front_end.ui.theme.Announcement.data.NoticeData
 
 class NoticeRVAdapter(private val context: Context,
@@ -16,14 +16,14 @@ class NoticeRVAdapter(private val context: Context,
             const val IMPORTANT = 1
         }
 
-    inner class ViewHolderNormal(private val binding: ItemAnnouncementNormalBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolderNormal(private val binding: ItemNoticeNormalBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: NoticeData){
             binding.tvNoticeTitle.text = item.title
             binding.tvNoticeDate.text = item.date
         }
     }
 
-    inner class ViewHolderImportant(private val binding: ItemAnnouncementImportantBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolderImportant(private val binding: ItemNoticeImportantBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: NoticeData){
             binding.tvNoticeTitle.text = item.title
             binding.tvNoticeDate.text = item.date
@@ -33,11 +33,11 @@ class NoticeRVAdapter(private val context: Context,
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when(viewType) {
             0 -> {
-                val binding = ItemAnnouncementNormalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                val binding = ItemNoticeNormalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
                 ViewHolderNormal(binding)
             }
             else -> {
-                val binding = ItemAnnouncementImportantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                val binding = ItemNoticeImportantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
                 ViewHolderImportant(binding)
             }
         }
--- app/src/main/java/com/ku_rum/front_end/ui/theme/Main/MainActivity.kt
+++ app/src/main/java/com/ku_rum/front_end/ui/theme/Main/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.ku_rum.front_end.ui.theme.Main
 
 import androidx.activity.OnBackPressedCallback
+import com.ku_rum.front_end.ui.theme.Announcement.AnnouncementFragment
 import com.ku_rum.front_end.BaseActivity
 import com.ku_rum.front_end.ui.theme.Map.MapFragment
 import com.ku_rum.front_end.R
 import com.ku_rum.front_end.ui.theme.Reservation.ReservationFragment
 import com.ku_rum.front_end.databinding.ActivityMainBinding
-import com.ku_rum.front_end.ui.theme.Announcement.AnnouncementFragment
 import com.ku_rum.front_end.ui.theme.My.MyFragment
 
 class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
