32c556bbea024d13522717845e1ccd7d1f0d9f75
wa4abi <heukimjalatte@gmail.com>
2025-01-05 04:34:25 +0900

    Fix: Announcement

--- app/src/main/java/com/ku_rum/front_end/ui/theme/Announcement/AnnouncementListFragment.kt
+++ app/src/main/java/com/ku_rum/front_end/ui/theme/Announcement/AnnouncementListFragment.kt
@@ -1,13 +1,9 @@
 package com.ku_rum.front_end.ui.theme.Announcement
 
-import android.content.Context
-import android.view.inputmethod.EditorInfo
 import com.example.ku_rum.Notice.adapter.NoticeRVAdapter
-import com.google.android.material.tabs.TabLayoutMediator
 import com.ku_rum.front_end.BaseFragment
 import com.ku_rum.front_end.databinding.FragmentAnnouncementListBinding
 import com.ku_rum.front_end.ui.theme.Announcement.data.NoticeData
-
 
 class AnnouncementListFragment : BaseFragment<FragmentAnnouncementListBinding>(FragmentAnnouncementListBinding::inflate) {
     private lateinit var noticeRVAdapter: NoticeRVAdapter
